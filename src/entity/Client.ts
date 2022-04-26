2e282f2d61f359c08c167f1d170734cf025ddd45
Arcdie <xdshkaa2@gmail.com>
2022-04-26 13:06:58 +0300

    fixes

--- src/entity/Client.ts
+++ src/entity/Client.ts
@@ -2,9 +2,10 @@ import {Entity, OneToOne} from 'typeorm';
 import {Contains} from 'class-validator';
 
 import {User} from './User';
+import {AbstractBaseEntity} from './AbstractBaseEntity';
 
 @Entity()
-export class Client extends User {
+export class Client extends AbstractBaseEntity {
   @Contains('client-')
   name: string;
 
